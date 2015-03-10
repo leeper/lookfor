40f97986874cd227dfcbc62f1533a9ac69476e4f
leeper <thosjleeper@gmail.com>
2015-03-10 22:44:12 +0100

    work on data.frames

--- R/lookin.R
+++ R/lookin.R
@@ -4,8 +4,13 @@
 }
 
 .in_names <- function(x, what, ignore.case = FALSE, ...) {
-    w <- which(grepl(what, names(x), ...))
-    structure(setNames(w, names(x)[w]), location = "names")
+    n <- names(x)
+    if(!is.null(n)) {
+        w <- which(grepl(what, n, ...))
+        structure(setNames(w, n[w]), location = "names")
+    } else {
+        structure(list(), location = "names")
+    }
 }
 
 .in_colnames <- function(x, what, ignore.case = FALSE, ...) {
@@ -19,8 +24,13 @@
 }
 
 .in_comment <- function(x, what, ignore.case = FALSE, ...) {
-    w <- which(grepl(what, comment(x), ...))
-    structure(setNames(w, comment(x)[w]), location = "comment")
+    cmt <- comment(x)
+    if(!is.null(cmt)) {
+        w <- which(grepl(what, cmt, ...))
+        structure(setNames(w, cmt), location = "comment")
+    } else {
+        structure(list(), location = "comment")
+    }
 }
 
 .in_levels <- function(x, what, ignore.case = FALSE, ...) {
@@ -75,17 +85,16 @@ lookin.factor <- function(x, what, ...){
 lookin.data.frame <- function(x, what, ...){
     if(class(x) != 'data.frame')
         stop("Object must be a data.frame")
-    c(.in_names(names(x), what, ...),
-      #.in_attributes(x, what, ...),
-      .in_comment(x, what, ...),
-      sapply(x, lookin, ...))
+    list(attributes = .in_attributes(x, what, ...),
+         comment = .in_comment(x, what, ...),
+         variables = sapply(x, lookin, what = what, ...))
 }
 
 lookin.list <- function(x, what, ...){
     c(.in_names(names(x), what, ...),
       #.in_attributes(x, what, ...),
       .in_comment(x, what, ...),
-      sapply(x, lookin, ...))
+      sapply(x, lookin, what = what, ...))
 }
 
 lookin.matrix <- function(x, what, ...){
