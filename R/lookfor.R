d4b395b5d8fa6ef629ce28bc26ae7a960e217f66
Thomas J. Leeper <thosjleeper@gmail.com>
2015-03-12 14:19:11 +0100

    experiment with parsing lookin list

--- R/lookfor.R
+++ R/lookfor.R
@@ -12,17 +12,17 @@ lookfor <- function(what, ls_opts = list(name = ".GlobalEnv"), ...){
     # look for objects in loaded namespaces
     ns <- loadedNamespaces()
     ns <- ns[!ns == ".GlobalEnv"]
-    n <- setNames(lapply(ns, function(x) lookin(ls(asNamespace(x)), what = what, ...)), ns)
+    ns_names <- lookin(ns, what, ...)
+    in_ns <- setNames(lapply(ns, function(x) lookin(ls(asNamespace(x)), what = what, ...)), ns)
     
     # return value should be a list with the string matching the search, along with details of its position
     # big challenge is doing this recursively because, e.g., lists of lists of dataframes would be really difficult to search
     
-    structure(list(environment = s1, objects = d, namespaces = ns, fromNamespaces = n), class = "lookfor")
+    structure(list(environment = s1, objects = d, namespaces = ns_names, fromNamespaces = in_ns), class = "lookfor")
 }
 
 # print.lookfor <- function(x, ...){
-#     # something like:
-#     cat('Item\t\tLocation\n')
+#     anywhere <- rapply(x, function(z) if(length(z)) TRUE else FALSE, how = "replace")
 #     for(i in seq_along(x))
 #         cat(x[[i]]$object, '\t\t', x[[i]]$location,'\n')
 #     return(invisible(x))
