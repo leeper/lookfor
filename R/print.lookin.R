a6d41a5b3d2d900de2cfa2ad7450548cc230af55
leeper <thosjleeper@gmail.com>
2015-03-16 08:03:54 +0100

    export print.lookin

--- R/print.lookin.R
+++ R/print.lookin.R
@@ -60,15 +60,65 @@ print.lookin.matrix <- function(x, ...){
 }
 
 print.lookin.environment <- function(x, ...){
-    
+    for(i in seq_along(x$environment)) {
+        print(x$environment[[i]])
+    }
+    if(length(x$comment)) {
+        message("Matches found for '",attributes(x)$what,"' in comment(", attributes(x)$object, "):", sep = "")
+        print(data.frame(Match = names(x$comment), Position = x$comment), row.names = FALSE)
+    }
     invisible(x)
 }
 
 print.lookin.function <- function(x, ...){
-    
+    if(length(x$arguments) | length(x$body) | length(x$comment)) {
+        if(length(x$arguments)) {
+            message("Matches found for '",attributes(x)$what,"' in arguments for function `", attributes(x)$object, "`:", sep = "")
+            print(data.frame(Match = names(x$arguments), Position = x$arguments), row.names = FALSE)
+        }
+        if(length(x$body)) {
+            message("Matches found for '",attributes(x)$what,"' in in body of function `", attributes(x)$object, "`:", sep = "")
+            print(data.frame(Match = names(x$body), Line = x$body), row.names = FALSE)
+        }
+        if(length(x$comment)) {
+            message("Matches found for '",attributes(x)$what,"' in comment(", attributes(x)$object, "):", sep = "")
+            print(data.frame(Match = names(x$comment), Position = x$comment), row.names = FALSE)
+        }
+    } else {
+        message("lookfor did not find '",attributes(x)$what,"' anywhere in `", attributes(x)$object, "`. Bummer!\n", sep = "")
+    }
     invisible(x)
 }
 
-#print.lookin.list <- function(x, ...){    
-#    invisible(x)
-#}
+print.lookin.list <- function(x, ...){    
+    a <- x$attributes
+    x$attributes <- NULL
+    b <- x$comment
+    x$comment <- NULL
+    
+    lv <- sapply(x, function(z) { if(length(z[["values"]]) | length(z[["comment"]])) { TRUE } else { FALSE } })
+    aa <- any(rapply(a, length))
+    ab <- any(rapply(b, length))
+    
+    if(any(lv) | aa | ab) {
+        if(any(lv)) {
+            message("Matches found for '",attributes(x)$what,"' in '", attributes(x)$object, "':", sep = "")
+            for(i in seq_along(x)) {
+                print(x[[i]])
+            }
+        }
+        if(aa) {
+            message("Matches found for '",attributes(x)$what,"' in attributes(", attributes(x)$object, "):", sep = "")
+            for(i in seq_along(a)) {
+                print(a[[i]])
+            }
+        }
+        if(ab) {
+            message("Matches found for '",attributes(x)$what,"' in comment(", attributes(x)$object, "):", sep = "")
+            print(data.frame(Match = names(b), Position = b), row.names = FALSE)
+        }
+    } else {
+        message("lookfor did not find '",attributes(x)$what,"' anywhere in '", attributes(x)$object, "'. Bummer!\n", sep = "")
+    }
+    invisible(x)
+}
